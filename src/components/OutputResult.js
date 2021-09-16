d01a32f0680ce86f36236a79dec484b622267990
Daniel Samuel <dansam5000@gmail.com>
2021-09-16 11:36:14 +0100

    Set proptypes and defaultProps on result-output

---
+++ src/components/OutputResult.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const OutputResult = ({ outputvalue }) => <div className="input-screen">{outputvalue}</div>;
+
+OutputResult.propTypes = {
+  outputvalue: PropTypes.string,
+};
+
+OutputResult.defaultProps = {
+  outputvalue: "",
+};
+
+export default OutputResult;
