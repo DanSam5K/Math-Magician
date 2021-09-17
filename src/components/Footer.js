51a9344e211cd7a75e1ac986b6195c28fb249e63
Daniel Samuel <dansam5000@gmail.com>
2021-09-17 07:35:34 +0100

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            <p className="footer">Designed By DanSam</p>
+            <p className="footer">LLC Microverse</p>
+            <p className="footer">2021</p>
+        </div>
+    )
+}
+
+export default Footer;
