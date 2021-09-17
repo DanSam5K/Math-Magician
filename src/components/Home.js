f7a959ef8ea8a4db49e65e0047e72733213938a9
Daniel Samuel <dansam5000@gmail.com>
2021-09-17 07:37:40 +0100

    Add Home and Quote component

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+ const Home = () => {
+    return (
+        <div>
+            <h2 className="homeTitle">Welcome to our page</h2>
+            <p className="homeParagraph">"Math magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to:
+               <ul>
+                 <li>Make simple calculations.</li>.
+                <li>Read a random math-related quote.</li>
+              </ul>
+            </p>   
+           <p>By building this simple application, I was able to learn how to use React.</p>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/Quote.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Quote = () => {
+    return (
+        <p className="quote">Mathematics is not about numbers, equations, computations, or algorithms: It`s about understanding. - Williams Paul</p>
+    )
+}
+
+export default Quote;
