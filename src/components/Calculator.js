f464897cfc300d801dfd31fab5de839b20a41f56
Daniel Samuel <dansam5000@gmail.com>
2021-09-17 06:40:45 +0100

    Refactor Calculator component to hooks & into functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,37 +1,46 @@
-import React from 'react';
+import React, { useState, setState } from 'react';
 import MagicBtn from './MagicBtn';
 import OutputResult from './OutputResult';
 import calculate from '../logic/calculate';
 import generateOutputValue from '../helper/generateOutputvalue';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Calculator = () => {
+  const [calculateState, calculateStateHandler] = useState(
+    {
+      total: null,
+      next: null,
+      operation: null,
+    },
+  );
 
-  clickEventListener = (event) => {
-    const object = this.state;
+  const clickEventListener = (event) => {
     const { value } = event.target;
-    const result = calculate(object, value);
-    this.setState(result);
-  }
+    let { total, next, operation } = calculate(calculateState, value);
+    if (total === undefined) {
+      total = setState(calculateState.total);
+    }
+    if (next === undefined) {
+      next = setState(calculateState.next);
+    }
+    if (operation === undefined) {
+      operation = setState(calculateState.operation);
+    }
+    calculateStateHandler({ total, next, operation });
+  };
 
-  render() {
-    const outputValue = generateOutputValue(this.state);
-    const buttonKeys = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '1', '='];
-    const buttons = buttonKeys.map((val) => (
-      <MagicBtn value={val} key={0} clickEventListener={this.clickEventListener} />
-    ));
-    return (
-      <div className="magician">
-        <OutputResult outputvalue={outputValue} />
-        <div className="magician-keys">
-          {buttons}
-        </div>
+  const outputValue = generateOutputValue(calculateState);
+  const buttonKeys = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '1', '='];
+  const buttons = buttonKeys.map((val) => (
+    <MagicBtn value={val} key={0} clickEventListener={clickEventListener} />
+  ));
+  return (
+    <div className="magician">
+      <OutputResult outputvalue={outputValue} />
+      <div className="magician-keys">
+        {buttons}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
