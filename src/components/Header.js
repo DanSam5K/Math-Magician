7a6e7f24eb3767ba5a9587ab6317a0e2360486ab
Daniel Samuel <dansam5000@gmail.com>
2021-09-22 02:13:09 +0100

    Add router Link to navigate app pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div className="container">
@@ -6,10 +7,10 @@ const Header = () => (
     <nav className="Nav">
       <ul className="flex">
         <li className="link">
-          {/* <Link className="link" to="/Home">Home</Link> */}
+          <Link className="link" to="/Home">Home</Link>
         </li>
         <li className="link">
-          {/* <Link className="link" to="/Calculator">Calculator</Link> */}
+          <Link className="link" to="/Calculator">Calculator</Link>
         </li>
         <li className="link">
           <Link className="link" to="/Quote">Quote</Link>
