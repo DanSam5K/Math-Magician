552a30e6073c08e8b97e7208569bb5f3d7901706
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-09-22 17:01:17 -0400

    Test substract operate

--- src/test/operate.test.js
+++ src/test/operate.test.js
@@ -4,4 +4,8 @@ describe('operate', () => {
   it('should return the sum of two numbers', () => {
     expect(operate('1', '2', '+')).toBe('3');
   });
+
+  it('should return the difference of two numbers', () => {
+    expect(operate('1', '2', '-')).toBe('-1');
+  });
 });
