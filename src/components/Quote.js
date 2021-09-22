6852e30064edafbb8ef21e237918619f4db76042
Daniel Samuel <dansam5000@gmail.com>
2021-09-22 01:45:24 +0100

    Modify app

--- src/components/Quote.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Quote = () => (
-  <p className="p-quote">Mathematics is not about numbers, equations, computations, or algorithms: It`s about understanding. - Williams Paul</p>
-);
-
-export default Quote;
