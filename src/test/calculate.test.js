e8e7f77646e6645d134c9e42b73559e4262d8e21
Daniel Samuel <dansam5000@gmail.com>
2021-09-22 20:48:42 +0100

    Add Zero button test

--- src/test/calculate.test.js
+++ src/test/calculate.test.js
@@ -4,6 +4,12 @@ test("AC button to clear output", () => {
    expect(calculate({total: '5', next: '7', operation: '+'}, 'AC')).toStrictEqual({total: null, next: null, operation: null});
 })
 
+test("Zero button to return empty output", () => {
+   expect(calculate({total: null, next: '0', operation: null}, '0')).toStrictEqual({});
+})
+
+
+
 
 
 
