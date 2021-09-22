8642bb674f9426b4956b74df0fe5e784526368e5
Daniel Samuel <dansam5000@gmail.com>
2021-09-22 23:19:37 +0100

    Install test renderer & add test HomeUI snapshots

--- src/test/snapshot.test.js
+++ src/test/snapshot.test.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Home from '../components/Home';
 import Quote from '../components/Quote';
 
 
-it('renders quote as intended', () => {
-   const UIQuote = renderer
-     .create(<Quote />).
-	 toJSON();
-	 expect(UIQuote).toThrowErrorMatchingInlineSnapshot();
-});
+
+describe('Components Snapshots test', () => {
+
+	it('renders home as intended', () => {
+	   const UIHome= renderer
+		 .create(<Home />)
+		 .toJSON();
+		 expect(UIHome).toMatchSnapshot();
+	});
+
+	it('renders quote as intended', () => {
+	   const UIQuote = renderer
+		 .create(<Quote />)
+		 .toJSON();
+		 expect(UIQuote).toMatchSnapshot();
+	});
+
+})
