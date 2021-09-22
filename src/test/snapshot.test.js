65e76fbdd3a06ff1d5d169f2679c1695a1040ece
Daniel Samuel <dansam5000@gmail.com>
2021-09-22 23:43:54 +0100

    Fix linters errors

--- src/test/snapshot.test.js
+++ src/test/snapshot.test.js
@@ -3,22 +3,14 @@ import renderer from 'react-test-renderer';
 import Home from '../components/Home';
 import Quote from '../components/Quote';
 
-
-
 describe('Components Snapshots test', () => {
+  it('renders home as intended', () => {
+    const UIHome = renderer.create(<Home />).toJSON();
+    expect(UIHome).toMatchSnapshot();
+  });
 
-	it('renders home as intended', () => {
-	   const UIHome= renderer
-		 .create(<Home />)
-		 .toJSON();
-		 expect(UIHome).toMatchSnapshot();
-	});
-
-	it('renders quote as intended', () => {
-	   const UIQuote = renderer
-		 .create(<Quote />)
-		 .toJSON();
-		 expect(UIQuote).toMatchSnapshot();
-	});
-
-})
+  it('renders quote as intended', () => {
+    const UIQuote = renderer.create(<Quote />).toJSON();
+    expect(UIQuote).toMatchSnapshot();
+  });
+});
